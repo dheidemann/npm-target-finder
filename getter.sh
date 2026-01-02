abfa4a8a0aff201660cff6e908923c5ba9a4beee
Daniel <mail@dheidemann.de>
2026-01-02 14:51:16 +0100

    data and fixes

--- getter.sh
+++ getter.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 
 GH_TOKEN=$1
 
-mkdir -p src/data
+(
+  cd src
+  npm i
+)
+
 GITHUB_TOKEN=$GH_TOKEN node src/fetch-gh-pkgs.js --output data/packages.csv --include-github
 GITHUB_TOKEN=$GH_TOKEN node src/fetch-maintainer-data.js data/packages.csv data/users.csv
 node src/fetch-gh-avg-open.js data/packages.csv data/avg_open.csv --token=$GH_TOKEN
--- max-influence.sh
+++ max-influence.sh
@@ -7,10 +7,10 @@ source venv/bin/activate
 pip install -r src/requirements.txt
 pip install PyQt6
 
-python flatten-dependencies.py -i data/packages.csv -o data/flattened_dependencies.csv
-python merge.py --identifier pkg_name --paths data/packages.csv,data/package_scores.csv --sort_by inactivity_score --output all_pkg_max_infl.csv
-python build-dependency-graph.py -e data/flattened_dependencies.csv -n data/all_pkg_max_infl.csv -o data/graph
+python src/flatten-dependencies.py -i data/packages.csv -o data/flattened_dependencies.csv
+python src/merge.py --identifier pkg_name --paths data/packages.csv,data/scores.csv --sort_by inactivity_score --output data/all_pkg_max_infl.csv
+python src/build-dependency-graph.py -e data/flattened_dependencies.csv -n data/all_pkg_max_infl.csv -o data/graph.gexf
 
 mkdir -p bin
 g++ -O3 -fopenmp src/weighted_max_influence.cc -o bin/influence
-./bin/influence src/data/graph.gexf 10 inactivity_score
+./bin/influence data/graph.gexf 10 inactivity_score
